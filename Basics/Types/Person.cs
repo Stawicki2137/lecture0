102a756b068dc031bf06a616a2a9b70b6c18554e
Mateusz <mateusz.stawicki212@gmail.com>
2024-10-13 19:42:51 +0200

    Types\Person

--- Basics/Types/Person.cs
+++ Basics/Types/Person.cs
@@ -2,12 +2,14 @@ namespace Types;
 
 public class Person
 {
-    public string Name; 
+    public string Name;
     public int Age;
+    public double Height;
 
-    public Person(string name, int age)
+    public Person(string name, int age, double height)
     {
         Name = name;
         Age = age;
+        Height = height;
     }
 }
