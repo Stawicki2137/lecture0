5cad1f15dc7089d35b8b7bc3cbb7fd0f13705064
Mateusz <mateusz.stawicki212@gmail.com>
2024-10-13 19:15:42 +0200

    Writting to the sandbox file

--- Basics/Sandbox/Program.cs
+++ Basics/Sandbox/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        string name1 = "Mateusz", name2 = "Karolinka";
+        for(int i=0; i<10; ++i)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"Hello, {name1}");
+            }
+            else
+            {
+                {
+                    Console.WriteLine($"Hello, {name2}");
+                }
+            }
+        }
 
     }
 }
